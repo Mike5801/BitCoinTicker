66dfa53f344f96c159fee806d8cea552bbca0468
Miguel Weiping Tang Feng <88734115+Mike5801@users.noreply.github.com>
2023-07-31 18:58:06 -0600

    add initial app setup

---
+++ bitcoin_ticker/lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:bitcoin_ticker/services/price_screen.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData.dark().copyWith(
+        primaryColor: Colors.lightBlue,
+        scaffoldBackgroundColor: Colors.white,
+      ),
+      debugShowCheckedModeBanner: false,
+      home: const PriceScreen(),
+    );
+  }
+}
